d52b76aa2f2f85ef8312a4c063f4a32d2fbaa480
calvinpete <tinkacalvin@gmail.com>
2018-08-18 21:20:21 +0300

    [Delivers #159868864] move config.py to instance folder

---
+++ instance/config.py
@@ -0,0 +1,24 @@
+
+class Config(object):
+    """Parent configuration class that contains general settings
+    as default settings for other classes to inherit.
+    """
+    DEBUG = False
+    SECRET = 'SECRET'
+
+
+class DevelopmentConfig(Config):
+    """Configurations for Development."""
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """Configurations for Testing."""
+    TESTING = True
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """Configurations for Production."""
+    DEBUG = False
+    TESTING = False
