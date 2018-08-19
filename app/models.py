f82c5ca8579e4ffe8cd8070ad39be6bc4c424ec0
calvinpete <tinkacalvin@gmail.com>
2018-08-19 10:15:12 +0300

    [Delivers #159870036] a QuestionsModel class to represent questions

---
+++ app/models.py
@@ -0,0 +1,7 @@
+class QuestionsModel(object):
+    """The class model represents the questions """
+
+    def __init__(self, question, answer):
+        self.question_id = 0
+        self.question = question
+        self.answer = answer
