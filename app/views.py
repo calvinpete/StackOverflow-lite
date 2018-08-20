91a2a095528a34bab39b3f0d8da943b9010f8522
calvinpete <tinkacalvin@gmail.com>
2018-08-20 15:59:36 +0300

    [Delivers #159870066] create post question route

--- app/views.py
+++ app/views.py
@@ -27,3 +27,28 @@ class QuestionManager(Resource):
             questions.append(qns_data[question].__getattribute__("qn_title"))
         return jsonify(questions)
 
+    def post(self):
+        """
+        This method checks if the request data isn't there or is there but a qn_title is missing
+        then return an error code 400.
+        Then assigns a qn_id using length of qns_data plus one while tolerating a missing qn_details field
+        and an empty list of answers to create a question be appended to the qns_data list.
+        :return: {'message': 'Question added'}, 201
+        """
+        if not request.json or 'qn_title' not in request.json:
+            abort(400)
+        question = QuestionsModel(
+            qns={
+                'qn_id': len(qns_data) + 1,
+                'qn_title': request.json['qn_title'],
+                'qn_details': request.json.get('qn_details', ""),
+                'answers': []
+            }
+        )
+        qns_data.append(question)
+        return jsonify({'message': 'Question added'}), 201
+
+
+api.add_resource(QuestionManager, '/questions')
+
+
