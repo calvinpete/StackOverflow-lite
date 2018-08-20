274ed6693ed3a2b3c36a370543c207df26426aee
calvinpete <tinkacalvin@gmail.com>
2018-08-20 13:47:51 +0300

    [Delivers #159870036] modify the Questionsmodel class attribute

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 class QuestionsModel(object):
-    """The class model represents the questions """
+    """The class model represents the questions.
+    The __dict__ attribute holds all per-instance attributes of the object.
+    """
 
-    def __init__(self, question, answer):
-        self.question_id = 0
-        self.question = question
-        self.answer = answer
+    def __init__(self, qns):
+        self.__dict__ = qns
