4860e31b183a694e7f5c5bee7269e5b0b0d85ea4
calvinpete <tinkacalvin@gmail.com>
2018-08-31 07:26:04 +0300

    remove dead code

--- app/models.py
+++
@@ -1,7 +0,0 @@
-class QuestionsModel(object):
-    """The class model represents the questions.
-    The __dict__ attribute holds all per-instance attributes of the object.
-    """
-
-    def __init__(self, qns):
-        self.__dict__ = qns
